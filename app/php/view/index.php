d8d939295f61f0644d138ee2d15776ac0b58f75f
csp63 <csp63@z031enfp-1418.infra.enc>
2018-03-27 09:59:06 +0200

    test requète sql

--- app/php/view/index.php
+++ app/php/view/index.php
@@ -1,5 +1,40 @@
-<?php
-    require "entete.html";
-    require "piedPage.html";
-?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>yo</title>
+    </head>
+    <body>
+        <?php
+        require "entete.html";
+        require "piedPage.html";
+
+
+        try{
+        $db = new PDO("pgsql:host=10.189.251.9;dbname=scp6", "scp6", "scp6");
+        }
+        catch(Exception $e){
+            die("erreur : ".$e->getMessage());
+        }
+        $qry = $db->prepare("SELECT * FROM categories;");
+        $qry->execute();
+        //$noms = $qry->fetch();
+        print_r($noms);
+        echo '<p id="catégorie">';
+        while($nom = $qry->fetch()){
+            echo $nom['description'].'<br/>';
+            //echo "nom : ".$_GET['categorie'];
+        }
+        echo '</p>';
+        ?>
+        
+    </body>
+</html>
+
+
 
