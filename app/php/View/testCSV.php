8144e2caa60781154160a23907b901959f2b31f3
csp20 <csp20@z031enfp-9314.infra.enc>
2018-04-03 10:02:18 +0200

    add Championnats.php add Creneaux add Matchs add Salles calendrier script add calendar js

---
+++ app/php/View/testCSV.php
@@ -0,0 +1,71 @@
+
+
+<body>
+
+<table border="2">
+		<tr>
+			<th>ENTITE</th>
+			<th>JOURNEE</th>
+			<th>MATCH</th>
+                        <th>DATE</th>
+                        <th>HEURE</th>
+                        <th>EQUIPE NO DOM</th>
+                        <th>EQUIPE NOM DOM</th>
+                        <th>EQUIPE NO EXT</th>
+                        <th>EQUIPE NOM EXT</th>
+                        <th>SET</th>
+                        <th>SCORE</th>
+                        <th>TOTAL</th>
+                        <th>SALLE</th>
+                        <th>ARBITRE 1</th>
+                        <th>ARBITRE 2</th>
+                        
+		</tr>
+	
+	<?php
+	$loc = "ffvb_calendrier.csv";
+	$fd = fopen("$loc","r"); 
+	$tab = array();
+	//$tab = list($user, $pass, $uid, $gid, $gecos, $home, $shell)
+	$tab2 = array();
+	if($fd == NULL)
+	{
+		echo "Erreur d'ouverture de : $fichier<br>";
+		exit(-1);
+	}
+	else
+	{
+		while (($buffer = fgets($fd)) != false) {
+			$tab2[] = $buffer;
+		}
+		
+		asort($tab2);
+		foreach ($tab2 as $str)
+		{
+			$tab = explode(';',$str);
+			echo "<tr>";
+			echo "<td>$tab[0]</td>";
+			echo "<td>$tab[1]</td>";
+			echo "<td>$tab[2]</td>";
+                        echo "<td>$tab[3]</td>";
+                        echo "<td>$tab[4]</td>";
+                        echo "<td>$tab[5]</td>";
+			echo "<td>$tab[6]</td>";
+			echo "<td>$tab[7]</td>";
+                        echo "<td>$tab[8]</td>";
+                        echo "<td>$tab[9]</td>";
+			echo "<td>$tab[10]</td>";
+			echo "<td>$tab[11]</td>";
+                        echo "<td>$tab[12]</td>";
+			echo "<td>$tab[13]</td>";
+			echo "<td>$tab[14]</td>";
+			echo "</tr>";
+		}
+		
+	}
+	?>	
+	</table>
+
+
+</body>
+</html> 
---
+++ public/fullcalendar-3.9.0/demos/agenda-views.php
@@ -0,0 +1,121 @@
+<?php
+ require_once "/../../pdo.php";
+   
+    $db = new Database();
+   
+
+      
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8' />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link href='../fullcalendar.min.css' rel='stylesheet' />
+<link href='../fullcalendar.print.min.css' rel='stylesheet' media='print' />
+<script src='../lib/moment.min.js'></script>
+<script src='../lib/jquery.min.js'></script>
+<script src='../fullcalendar.min.js'></script>
+<script>
+
+  $(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      defaultDate: '2018-03-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2018-03-01',
+        },
+        {
+          title: 'Long Event',
+          start: '2018-03-07',
+          end: '2018-03-10'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2018-03-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2018-03-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2018-03-11',
+          end: '2018-03-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2018-03-12T10:30:00',
+          end: '2018-03-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2018-03-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2018-03-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2018-03-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2018-03-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2018-03-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2018-03-28'
+        }
+      ]
+    });
+
+  });
+
+</script>
+<style>
+
+  body {
+    margin: 40px 10px;
+    padding: 0;
+    font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+    font-size: 14px;
+  }
+
+  #calendar {
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+</style>
+</head>
+
+<body>
+    <?php include('menu.php'); ?>
+
+  <div id='calendar'></div>
+
+</body>
+</html>
