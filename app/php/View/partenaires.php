7e3e283e7d201ec09da2f44f6699ad687350ef33
csp12 <csp12@z031enfp-9322.infra.enc>
2018-04-03 11:48:46 +0200

    modif partenaire

---
+++ app/php/View/partenaires.php
@@ -0,0 +1,107 @@
+<?php include('entete.html'); ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        
+
+    </head>
+    <body>
+        <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+                    <script type="text/javascript" src="carousel.js"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    </body>
+
+
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container-fluid">
+    <div id="custom_carousel" class="carousel slide" data-ride="carousel" data-interval="2500">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3"><img src="../../../public/img/partenaires/la-part-de-reve.jpg" class="img-responsive"></div>
+                        <div class="col-md-9">
+                            <h2>La part de rêve</h2>
+                            <a href="http://www.lapartdereve.fr/">lapartdereve.fr</a> 
+                            <br>
+                        </div>
+                    </div>
+                </div>            
+            </div> 
+            <div class="item">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3"><img src="../../../public/img/partenaires/l-air-du-temps.jpg" class="img-responsive"></div>
+                        <div class="col-md-9">
+                            <h2>L'air du temps</h2>
+                            <p>Centre Commercial « La Rivière  » <br>5 bis rue Pierre Loti <br> 31830 PLAISANCE DU TOUCH</p>
+                            <a href="http://air-du-temps.fr/">air-du-temps.fr</a>
+                             
+                        </div>
+                    </div>
+                </div>            
+            </div> 
+            <div class="item">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3"><img src="../../../public/img/partenaires/Vue_de_reve.png" class="img-responsive"></div>
+                        <div class="col-md-9">
+                            <h2>Vue de rêve</h2>
+                            <p>Centre Commercial « La Rivière  » <br>5 bis rue Pierre Loti <br> 31830 PLAISANCE DU TOUCH</p>
+                            <a href="http://vuedereve.fr/">vuedereve.fr</a>
+                        </div>
+                    </div>
+                </div>           
+            </div> 
+            <div class="item">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3"><img src="../../../public/img/partenaires/ifca.png" class="img-responsive"></div>
+                        <div class="col-md-9">
+                            <h2>IFCA</h2>
+                            <p>197 avenue de Fronton <br> 31200 TOULOUSE</p>
+                            <a href="http://ifca31.fr/">ifca31.fr</a>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                </div>           
+            </div> 
+                        <div class="item">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3"><img src="../../../public/img/partenaires/pouria-ardalan.jpg" class="img-responsive"></div>
+                        <div class="col-md-9">
+                            <h2>Pouria Ardalan</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, labore, magni illum nemo ipsum quod voluptates ab natus nulla possimus incidunt aut neque quaerat mollitia perspiciatis assumenda asperiores consequatur soluta.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, labore, magni illum nemo ipsum quod voluptates ab natus nulla possimus incidunt aut neque quaerat mollitia perspiciatis assumenda asperiores consequatur soluta.</p>
+                        </div>
+                    </div>
+                </div>            
+            </div> 
+        <!-- End Item -->
+        </div>
+        <!-- End Carousel Inner -->
+        <div class="controls">
+            <ul class="nav">
+                <li data-target="#custom_carousel" data-slide-to="0" class="active"><a href="#"><img src="../../../public/img/partenaires/la-part-de-reve.jpg" style="width:50px; height:50px;"><small> La part de rêve</small></a></li>
+                <li data-target="#custom_carousel" data-slide-to="1"><a href="#"><img src="../../../public/img/partenaires/l-air-du-temps.jpg" style="width:50px; height:50px;"><small> L'air du temps</small></a></li>
+                <li data-target="#custom_carousel" data-slide-to="2"><a href="#"><img src="../../../public/img/partenaires/Vue_de_reve.png" style="width:50px; height:50px;"><small> Vue de rêve</small></a></li>
+                <li data-target="#custom_carousel" data-slide-to="3"><a href="#"><img src="../../../public/img/partenaires/ifca.png" style="width:50px; height:50px;"><small> IFCA</small></a></li>
+                <li data-target="#custom_carousel" data-slide-to="4"><a href="#"><img src="../../../public/img/partenaires/pouria-ardalan.jpg" style="width:50px; height:50px;"><small> Pouria Ardalan</small></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- End Carousel -->
+</div>
+
+</html>
