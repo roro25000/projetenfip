6c4d34f75ff33b7d40e3e07e4831a71b0fe9ba0e
csp12 <csp12@z031enfp-9322.infra.enc>
2018-04-06 10:22:01 +0200

    modif navbar

--- app/php/View/entete.php
+++ app/php/View/entete.php
@@ -7,12 +7,11 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="../../../public/css/stylevolley.css" type="text/css" media="screen" >
-               
-        <title>Hello, world!</title>
+
+        <title>SCP VOLLEY</title>
 
     </head>
     <body>
-        <h1>SCP VOLLEY</h1>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"><img src="../../../public/img/equipe/logo scp volley.jpg" width="50" height="50"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,30 +29,20 @@
                     <li class="nav-item">
                         <a class="nav-link" href="contact.php">Contact</a>
                     </li>
+
                     <li class="nav-item">
-                        <a class="nav-link" href="espaceadherent.php">Espace Adhérents</a>
-                    </li>
-                     <li class="nav-item">
-                         <a class="nav-link" href="partenaires.php">Partenaires</a>
+                        <a class="nav-link" href="partenaires.php">Partenaires</a>
                     </li>
 
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Choix equipe
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Equipe 1</a>
-                            <a class="dropdown-item" href="#">Equipe 2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Equipe 3</a>
-                        </div>
+                   
+                    <li class="nav-item">
+                        <a class="nav-link" align="right" href="espaceadherent.php" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" align="right" href="index.php" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-out"></span> Déconnexion</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+
 
 
 
