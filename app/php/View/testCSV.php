9a39f400671f30e7c66322754190cccd5493341c
csp20 <csp20@z031enfp-9314.infra.enc>
2018-04-06 08:46:14 +0200

    test CSV

--- app/php/View/testCSV.php
+++ app/php/View/testCSV.php
@@ -23,7 +23,7 @@
 		</tr>
 	
 	<?php
-	$loc = "ffvb_calendrier.csv";
+	$loc = "../../../public/ressources/csv/ffvb_calendrier.csv";
 	$fd = fopen("$loc","r"); 
 	$tab = array();
 	//$tab = list($user, $pass, $uid, $gid, $gecos, $home, $shell)
