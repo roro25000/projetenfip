f81ed0ca4e6cf59b2184fdfd3c57bfbda3a8af46
csp12 <csp12@z031enfp-9322.infra.enc>
2018-05-04 11:34:23 +0200

    accueil

--- app/php/View/index.php
+++ app/php/View/index.php
@@ -1,9 +1,57 @@
+
 <?php
 //test
 //        phpinfo();
 require "entete.php";
-require "piedPage.php";
 ?>
 
+    <link href="../../../public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+    <link href="../../../public/css/business-casual.min.css" rel="stylesheet">
 
+    <h2 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-lower">Sporting Club de Plaisance Volley</span>
+    </h2>
+    
+    <section class="page-section clearfix">
+      <div class="container">
+        <div class="intro">
+          <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="../../../public/img/equipe/FSGT.jpg" alt="">
+          <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-lower">Présentation</span>
+            </h2>
+            <p class="mb-3">Créée en 1987, le club se classe dans les 5 premiers Clubs de Volley-Ball de la région, avec un effectif actuel supérieur à 100 joueurs. Il engage des équipes compétitions filles et garçons à partir de 5 ans, dans quasiment toutes les catégories, ainsi que des équipes loisirs. Pour la saison 2017-2018, la section à obtenu le label de club formateur Futur de la FFBV.
+            </p>
+            <div class="intro-button mx-auto">
+              <a class="btn btn-primary btn-xl" href="#">Rejoignez-nous !</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
+    <section class="page-section cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="cta-inner text-center rounded">
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-lower">Calendrier</span>
+              </h2>
+              <p class="mb-0">Votre club vit grâce à l’implication de bénévoles. Vous avez un peu de temps libre? Vous souhaitez vous investir auprès de jeunes? Venez nous rencontrer!
+Afin de faciliter les premiers pas dans l’entrainement, le club vous accompagne tout au long de la saison. Les entraîneurs bénévoles qui le souhaitent pourront être assistés pendant leurs entraînements par des entraîneurs plus expérimentés. De plus, des 1/2 journées de formation seront proposées régulièrement tout au long de l’année.
+Si la formation de nos jeunes vous intéresse, n’hésitez pas à prendre contact avec nous et à venir participer à quelques séances d’entraînements.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <!-- Bootstrap core JavaScript -->
+    <script src="../../../public/vendor/jquery/jquery.min.js"></script>
+    <script src="../../../public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<?php require "piedPage.php";?>
