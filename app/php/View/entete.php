ed4ccef26c57cf30b86e93d1ce78a61e007c9b84
csp68 <csp68@z031enfp-1405.infra.enc>
2018-05-14 14:11:01 +0200

    Max est une branque

--- app/php/View/entete.php
+++ app/php/View/entete.php
@@ -9,10 +9,8 @@
         <link rel="stylesheet" href="../../../public/ionicons-2.0.1/css/ionicons.min.css">
  
         <link rel="stylesheet" href="../../../public/bootstrap-4.0.0/dist/css/bootstrap.min.css">
-        <link href="../../../public/css/business-casual.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../../../public/css/stylevolley.css" type="text/css" media="screen" >
 
-     
-        
         <title>SCP VOLLEY</title>
 
     </head>
@@ -20,8 +18,8 @@
         <?php
         session_start();
         ?>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light entete">
-            <a class="navbar-brand" href="index.php"><img src="../../../public/img/equipe/logo scp volley.jpg" width="50" height="50"></a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img src="../../../public/img/equipe/logo scp volley.jpg" width="50" height="50"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -38,16 +36,23 @@
                         <a class="nav-link" href="contact.php">Contact</a>
                     </li>
 
+                    <li class="nav-item">
+                        <a class="nav-link" href="partenaires.php">Partenaires</a>
+                    </li>
                     <?php
                     if (isset($_SESSION['identifiant'])) {
-                    ?>    
-                   
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="espaceadherent.php">Administration</a>
+                    </li>
+                    </ul><ul class="nav navbar-nav navbar-right">
                     <li class="nav-item">
                         <form id="deconnexionForm" action="espaceadherent.php" method="POST">
                             <input type="hidden" value="deconnexion" name="deconnexion">
-                            <a class="nav-link" class="navbar-right" role="button" aria-haspopup="true" aria-expanded="false" href="javascript:{}" onclick="document.getElementById('deconnexionForm').submit();"><span class="ion-person active"></span> Deconnexion</a>
+                            <a class="nav-link" align="right" role="button" aria-haspopup="true" aria-expanded="false" href="javascript:{}" onclick="document.getElementById('deconnexionForm').submit();"><span class="glyphicon glyphicon-user"></span> Deconnexion</a>
                         </form>
                     </li>
+                    </ul>
                     <?php
                     } else {
                     echo '</ul><ul class="nav navbar-nav navbar-right">
--- app/php/View/piedPage.php
+++ app/php/View/piedPage.php
@@ -1,42 +1,9 @@
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="../../../public/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block" src="../../../public/img/partenaires/la-part-de-reve.jpg" alt="First slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/l-air-du-temps.jpg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/Vue_de_reve.png" alt="Third slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/l-abattoir.jpg" alt="4 slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/pouria-ardalan.jpg" alt="5 slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/rei.jpg" alt="sixth slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/ifca.png" alt="7 slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/afsom.jpg" alt="8 slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="../../../public/img/partenaires/eritec.png" alt="9 slide">
-        </div>
-    </div>
+</html>
 
 
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
