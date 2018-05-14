c62b18fd5833aeccc6b94bd90e0ceed372588d6e
csp68 <csp68@z031enfp-1405.infra.enc>
2018-05-14 14:55:12 +0200

    ON GARDE LES SCRIPTS QUAND ON COMMIT !!!!!

--- app/php/View/entete.php
+++ app/php/View/entete.php
@@ -37,6 +37,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="contact.php">Contact</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="gestionAdherent.php">Administration</a>
+                    </li>
 
                     <?php
                     if (isset($_SESSION['identifiant'])) {
